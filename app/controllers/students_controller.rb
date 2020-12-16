9e10dafe09216873036534c7e11eca48b26271be
MaryAnnN28 <maryann.navarrete@gmail.com>
2020-12-16 10:05:14 -0500

    commit tree 3471f0f0ef6eb9f6b5156f91ec2d96d19d8f4f13

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,16 +1,33 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
+  before_action :set_student, only: [:show, :update]
+  
   
   def index
     @students = Student.all
   end
 
   def show
+    @student = Student.find(params[:id])
+    if @student.active == false
+      @activity = "This student is currently inactive"
+    else 
+      @activity = "This student is currently active."
+    end
   end
+
+  def update
+    @student.toggle!(:active)
+    redirect_to student_path(@student)
+  end
+
+
+
+
 
   private
 
     def set_student
       @student = Student.find(params[:id])
     end
+
 end
